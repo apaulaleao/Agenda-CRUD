c3c8159c27c5a967fc4f04df26a9ae108a0b4aa6
Ana Paula Leão <paula.leao@gmail.com>
2022-06-28 00:33:09 -0300

    Adicionar arquivos de projeto.

---
+++ Context/AgendaContext.cs
@@ -0,0 +1,16 @@
+﻿using Agenda.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Agenda.Context
+{
+    public class AgendaContext : DbContext
+    {
+        public AgendaContext(DbContextOptions<AgendaContext> options) : base(options)
+        {
+
+        }
+        //faz o vinculo com o banco de dados( sao as classes criadas)
+        public DbSet<Contato> Contatos { get; set; }
+        
+    }
+}
---
+++ Models/Contato.cs
@@ -0,0 +1,10 @@
+﻿namespace Agenda.Models
+{
+    public class Contato
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime DataNascimento { get; set; }
+    }
+}
