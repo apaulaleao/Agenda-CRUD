fe8ce2566a0d53949b8c69e5be5da75df3c4b00a
Ana Paula Leão <paula.leao@gmail.com>
2022-06-29 13:27:23 -0300

    atualizacao

---
+++ Models/Endereco.cs
@@ -0,0 +1,16 @@
+﻿namespace Agenda.Models
+{
+    public class Endereco
+    {
+        public int Id { get; set; }
+        public string? Logradouro { get; set; }//null
+        public int? Numero { get; set; }
+        public string? Bairro { get; set; }
+        public string? Cidade { get; set; }
+
+        //1 contato pode ter um endereço, 1 endereço tem varios contato.
+            //1:1
+            // 1 endereço  pode ter 1 UF , 1 UF pode ter varios endereços
+            //1:n
+    }
+}
---
+++ Models/UF.cs
@@ -0,0 +1,9 @@
+﻿namespace Agenda.Models
+{
+    public class UF
+    {
+        public int Id { get; set; }
+        public string Sigla { get; set; }
+        public string Nome { get; set; }
+    }
+}
